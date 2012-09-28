54d614a5f3fd3be5be68c1a3734819e4153edbff
Cory Kaufman-Schofield <cory@corykaufman.com>
2012-09-28 08:31:11 -0400

    Make keyboard navigation more reliable

--- jquery.multiselect.js
+++ jquery.multiselect.js
@@ -1,27 +1,29 @@
 (function($) {
+
   $.multiselect = function(input, options) {
     var plugin = this, $input = $(input), settings,
     defaults = {
       render: function(id,label,index) {
-        return $('<li>').attr('data-multiselect-id',id).append('<a>'+label+'</a>');
+        return $('<li>').attr('data-multiselect-id',id).append(label);
       },
       numResults: 10,
-      allowAdd: false
+      allowAdd: true,
+      clearOnReset: false
     };
 
     plugin.init = function() {
+      plugin.defaults = defaults
       plugin.settings = settings = $.extend({}, defaults, options);
       $input.attr('autocomplete','off');
-      $list = $('<ul>').addClass('multiselect-results').insertAfter($input)
+      $list = plugin.getResultsWrapper();
+      $list.addClass('multiselect-results');
       $input.on('focus.multiselect', function(e) {
         $list.one('click.multiselect','li', function(e) {
           $list.unbind('blur.multiselect');
           plugin.select($(this).data('multiselect-id')).reset();
         });
         $input.one('blur.multiselect', function(e) {
-          setTimeout(function() {
-            plugin.reset()
-          },100);
+          setTimeout(plugin.blur,100);
         });
         $input.on('keydown.multiselect', function(e) {
           return plugin.keydown(e);
@@ -33,32 +35,45 @@
     };
 
     plugin.keydown = function(e) {
+      $input.off('keydown.multiselect');
+      setTimeout(function() {
+        $input.on('keydown.multiselect', function(e) {
+          return plugin.keydown(e);
+        });
+      },100);
       if (e.keyCode == 27) {
         return plugin.reset();
       }
+      /* If the user presses enter or tab, select current tab */
       if (e.keyCode == 13 || e.keyCode == 9) {
         selected = $list.children('li.selected')
         if (selected.length != 0) {
           plugin.select(selected.data('multiselect-id'));
         }
-        return plugin.reset();
+        if (e.keyCode == 13) {
+          return plugin.reset();
+        } else {
+          return plugin.blur();
+        }
       }
+      /* If the user presses the down arrow, highlight next tab */
       if (e.keyCode == 40) {
-        var selected = $list.children('li.selected');
-        if (selected[0] != $list.children('li:last')[0]) {
-          $next = selected.removeClass('selected').next('li')
+        var $selected = $list.children('li.selected');
+        if ($selected[0] != $list.children('li:last')[0]) {
+          $next = $selected.removeClass('selected').next('li')
           $next.addClass('selected');
-          $input.val($next.children('a').html())
+          // $input.val(settings.value($next.data('multiselect-id')));
         }
         return false;
       }
+      /* If the user presses the up arrow, highlight previous tab */
       if (e.keyCode == 38) {
         var $selected = $list.children('li.selected');
         if ($selected[0] != $list.children('li:first')[0]) {
           $selected.removeClass('selected');
           $prev = $selected.prev('li');
           $prev.addClass('selected');
-          $input.val($prev.children('a').html());
+          // $input.val(settings.value($prev.data('multiselect-id')));
         }
         return false;
       }
@@ -84,7 +99,7 @@
       });
 
       if (settings.allowAdd && $.inArray($input.val(),matches) == -1) {
-        $list.prepend(settings.render('multiselect-new-id',$input.val()+' (Add Artist)', -1));
+        $list.append(settings.render('multiselect-new-id','Add artist: ' + $input.val(), -1));
       }
       $list.children('li:first').addClass('selected');
       return false;
@@ -100,10 +115,30 @@
     }
 
     plugin.reset = function() {
-      $input.val('').blur();
       $list.empty();
-      $input.off('keydown.multiselect keyup.multiselect');
+      if (settings.clearOnReset) {
+        $input.val('');
+      }
       return false;
+    }
+
+    plugin.blur = function() {
+      plugin.reset();
+      $input.blur().off('keydown.multiselect keyup.multiselect');
+      return false;
+    }
+
+    plugin.getResultsWrapper = function() {
+      if (settings.resultsWrapper) {
+        $list = $(settings.resultsWrapper);
+        if (!$list.length) {
+          $.error('Could not find results wrapper with selector ' + settings.resultsWrapper);
+        }
+      } else {
+        $list = $('<ul>').insertAfter($input);
+      }
+      $list.addClass('multiselect-results');
+      return $list;
     }
 
     var fire = function(obj, name, data) {
