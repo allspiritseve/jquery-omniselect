4d7b387cc5a8c147156139e4671a2f211f5ff31d
Kevin Champion <kevin.champion@gmail.com>
2013-04-13 15:33:26 -0400

    Add cut and paste events.
    This handles the case where the mouse is used to cut and paste
    and thus the keypress events don't fire. If cut fires and the
    input is empty, the autocomplete hides. If paste fires and the
    input is not empty, the autocomplete renders.
    
    *** This uses a hackish technique of setting a 0ms timeout to
    get around the problem of the cut and paste events occuring
    before the actual cut/paste happens. As a result the value of
    the input after the cut/paste is not available. Setting a
    timeout gets around this problem, but a better approach would
    be to create after events for cut and paste.

--- jquery.omniselect.js
+++ jquery.omniselect.js
@@ -3,7 +3,7 @@
   "use strict";
 
   var omniselect = function(spec) {
-    
+
     var defaults = {
       source: [],
       resultsClass: 'omniselect-results',
@@ -25,6 +25,8 @@
       $input.on('keypress.omniselect', keypress);
       $input.on('keyup.omniselect', keyup);
       $input.on('keydown.omniselect', keydown);
+      $input.on('cut.omniselect', cut);
+      $input.on('paste.omniselect', paste);
 
       $results.on('click.omniselect', click);
       $results.on('mouseenter.omniselect', 'li', mouseenter);
@@ -226,6 +228,18 @@
       mouseover = false;
       if (!focused && visible) hide();
     };
+
+    var cut = function(e) {
+      setTimeout( function() {
+        if (visible && $input.val() == '') hide();
+      }, 0 );
+    }
+
+    var paste = function(e) {
+      setTimeout( function() {
+        if (!visible && $input.val() != '') render();
+      }, 0 );
+    }
 
     var fire = function(name, data) {
       var event = $.Event(name);
